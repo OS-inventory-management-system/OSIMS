815affb15ab849d4ec49d0f2d8e9097645f0ac88
Jamie Tait <taitjamie@outlook.com>
2025-06-20 09:21:09 -0700

    controller logic for login complete

--- server/server.ts
+++ server/server.ts
@@ -2,14 +2,16 @@ import express from 'express'
 import cors from 'cors'
 import {Client} from 'pg'
 
-// const client = new Client({
-//     user: 'postgres',
-//     password: 'hanshotfirst21',
-//     port:5432,
-//     host: 'ims.c29y4e4eay89.us-east-1.rds.amazonaws.com',
-//     database: 'postgres',
-//     ssl: {rejectUnauthorized:false}
-// })
+// console.log('Server starting...');
+
+const client = new Client({
+    user: 'postgres',
+    password: 'hanshotfirst21',
+    port:5432,
+    host: 'ims.c29y4e4eay89.us-east-1.rds.amazonaws.com',
+    database: 'postgres',
+    ssl: {rejectUnauthorized:false}
+})
 
 const app = express()
 
@@ -18,19 +20,26 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors())
 
-// async function connect() {
-//   try {
-//     await client.connect();
-//     const res = await client.query('SELECT NOW()');
-//     console.log('Connected:', res.rows[0]);
-//   } catch (err) {
-//     console.error('Connection error:', err);
-//   } finally {
-//     await client.end();
-//   }
-// }
+async function connect() {
 
-// connect()
+  try {
+        console.log('trying to connect')
+    await client.connect();
+
+    const res = await client.query('SELECT NOW()');
+
+    console.log('Connected:', res.rows[0]);
+  } catch (err) {
+    console.log('err')
+    console.error('Connection error:', err);
+  } finally {
+    await client.end();
+  }
+}
+
+
 
 const port = 3000
 app.listen(port, ()=> console.log(`Server listening on port ${port}`))
+
+connect()
